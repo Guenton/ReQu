b50009899518356216ce05c52ba3ba4ba85564f6
Guenton Brooks <guenton@gmail.com>
2019-05-27 07:23:21 -0400

    Starting to work on Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 
 const Navbar = () => {
   return (
     <div className="Navbar">
       <AppBar position="static" color="default">
-        <h1>Hello World</h1>
+        <Toolbar>
+          <h1>Hello World</h1>
+        </Toolbar>       
       </AppBar>
     </div>
   );
